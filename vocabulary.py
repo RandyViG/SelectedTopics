87502243debb417d7d9fcacf095215c478070c13
RandyVig <rvillanuevag1603@alumno.ipn.mx>
2020-02-18 11:01:36 -0600

    Organizando archivos

--- vocabulary.py
+++
@@ -1,9 +0,0 @@
-from nltk.book import text3
-
-vocabulary = sorted(set(text3))
-
-print('Num de Palabras: {}'.format(len(vocabulary)) )
-with open('vocabularyText3.txt', 'w') as f:
-    for word in vocabulary:
-        f.write(word+'\n')
-    f.close()
